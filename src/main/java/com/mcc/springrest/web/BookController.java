78989f52888a7074945f5eca3c271821d3aa3450
Michael C. Clark <mike@knowledgevision.com>
2013-06-02 08:16:01 -0400

    Update post with 201 and responsebody

--- src/main/java/com/mcc/springrest/web/BookController.java
+++ src/main/java/com/mcc/springrest/web/BookController.java
@@ -3,10 +3,13 @@ package com.mcc.springrest.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.mcc.springrest.domain.Book;
 import com.mcc.springrest.service.BookService;
@@ -16,8 +19,20 @@ public class BookController {
 
 	@Autowired BookService bookService;
 	
+	@RequestMapping(value="/books", method=RequestMethod.PUT)
+	public @ResponseBody Book updateBook( @RequestBody Book book ){
+		System.out.println("Book posted:"+book.getTitle());
+		return book;
+	}
+	
+	@RequestMapping(value="/books", method=RequestMethod.POST)
+	@ResponseStatus( value=HttpStatus.CREATED)
+	public @ResponseBody Book addBook( @RequestBody Book book ){
+		System.out.println("Book posted:"+book.getTitle());
+		return book;
+	}
+	
 	@RequestMapping( value="/books", method=RequestMethod.GET) 
-
 	public @ResponseBody List<Book> getAllBooks(){
 		return bookService.retrieveAllBooks();
 	}
